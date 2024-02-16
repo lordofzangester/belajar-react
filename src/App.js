3fcac747f8b1766d6749a8dae818fddbb7ac344c
Muhammad Rifqi Al Fauzan <rifqialfauzan03@gmail.com>
2024-02-16 18:29:48 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './component/Layout';
+// import {Routes, Route} from 'react-router-dom';
 
 function App() {
+  function onClickHandler() {
+    const input = document.getElementById("name");
+    alert(`Hello, ${input.value}!`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Layout></Layout>
+          <input type="text" id="name" placeholder="name"/>
+          <input onClick={onClickHandler} type="button" id="btn" value="Submit"/>
+      </div>
   );
 }
 
---
+++ src/component/Layout/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../Navbar/Navbar';
+export default function Layout() {
+    return <>
+        <div>
+            <Navbar />
+        </div>
+    </>
+}
+
+
